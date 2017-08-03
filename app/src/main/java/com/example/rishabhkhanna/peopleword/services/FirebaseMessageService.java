ec510876d6df6b101c18e03733a932cba00d902b
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-03 18:59:33 +0530

    priority for notification to pop on screen
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/services/FirebaseMessageService.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/services/FirebaseMessageService.java
@@ -1,5 +1,6 @@
 package com.example.rishabhkhanna.peopleword.services;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -36,46 +37,57 @@ import static com.example.rishabhkhanna.peopleword.utils.UtilMethods.getTopics;
 public class FirebaseMessageService extends FirebaseMessagingService {
     public static final String TAG = "FirebaseMessageService";
     public static int notificationId = 0;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        if(remoteMessage.getData().size() > 0 ){
+        if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "onMessageReceived: " + remoteMessage.getData());
             Bitmap bitmap = null;
-            RemoteViews remoteView = new RemoteViews(getApplicationContext().getPackageName(),R.layout.notification_layout);
-            RemoteViews remoteBigView = new RemoteViews(getApplicationContext().getPackageName(),R.layout.notification_expanded_layout);
+            RemoteViews remoteView = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification_layout);
+            RemoteViews remoteBigView = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification_expanded_layout);
 
             try {
-                 bitmap = Picasso.with(this).load(UtilMethods.getImageurl(remoteMessage.getData().get("image"))).get();
+                bitmap = Picasso.with(this).load(UtilMethods.getImageurl(remoteMessage.getData().get("image"))).get();
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            remoteView.setTextViewText(R.id.tvNotificationHeading,remoteMessage.getData().get("title"));
-            remoteBigView.setTextViewText(R.id.tvNotification,remoteMessage.getData().get("title"));
-            remoteBigView.setImageViewBitmap(R.id.imNotification,bitmap);
+            remoteView.setTextViewText(R.id.tvNotificationHeading, remoteMessage.getData().get("title"));
+            remoteBigView.setTextViewText(R.id.tvNotification, remoteMessage.getData().get("title"));
+            remoteBigView.setImageViewBitmap(R.id.imNotification, bitmap);
 
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm aa");
             String currentTime = simpleDateFormat.format(Calendar.getInstance().getTime());
-            remoteBigView.setTextViewText(R.id.tvExNotificationTime,currentTime);
+            remoteBigView.setTextViewText(R.id.tvExNotificationTime, currentTime);
             Log.d(TAG, "onMessageReceived: Current Time" + currentTime);
-            remoteView.setTextViewText(R.id.tvNotificationTime,currentTime);
-            NotificationCompat.Builder builder  = new NotificationCompat.Builder(this)
+            remoteView.setTextViewText(R.id.tvNotificationTime, currentTime);
+            NotificationCompat.Builder builder;
+            builder = new NotificationCompat.Builder(this)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setDefaults(Notification.DEFAULT_VIBRATE)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentTitle(remoteMessage.getData().get("title"))
-//                    .setCustomContentView(remoteView)
+//                        .setCustomContentView(remoteView)
                     .setCustomBigContentView(remoteBigView);
 
-            Intent intent = new Intent(this,BaseActivity.class);
+//                builder = new NotificationCompat.Builder(this)
+//                        .setSmallIcon(R.mipmap.ic_launcher)
+//                        .setContentTitle(remoteMessage.getData().get("title"))
+//                        .setCustomContentView(remoteView)
+//                        .setCustomBigContentView(remoteBigView);
+
+
+            Intent intent = new Intent(this, BaseActivity.class);
             Log.d(TAG, "onMessageReceived: " + remoteMessage.getData().get("news_id"));
-            intent.putExtra("notification_key",remoteMessage.getData().get("news_id"));
-            PendingIntent pendingIntent = PendingIntent.getActivity(this,notificationId,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            intent.putExtra("notification_key", remoteMessage.getData().get("news_id"));
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             builder.setContentIntent(pendingIntent);
             builder.setAutoCancel(true);
 
             notificationId = new Random().nextInt();
             NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             Log.d(TAG, "onMessageReceived: " + notificationId);
-            notificationManager.notify(notificationId,builder.build());
+            notificationManager.notify(notificationId, builder.build());
         }
         super.onMessageReceived(remoteMessage);
     }
