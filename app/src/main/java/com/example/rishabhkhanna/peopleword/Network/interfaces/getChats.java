9adbf4bd9155166e1d4b82b77aa8205fb9401125
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-07 12:40:03 +0530

    chat message layout
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Network/interfaces/getChats.java
@@ -0,0 +1,11 @@
+package com.example.rishabhkhanna.peopleword.Network.interfaces;
+
+import retrofit2.http.GET;
+
+/**
+ * Created by rishabhkhanna on 07/08/17.
+ */
+
+public interface getChats {
+    @GET("")
+}
