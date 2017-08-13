76ca286775569cbb9c58e682ebb2adb9cbb05adc
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-14 01:53:46 +0530

    removed unused statements
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/views/Activities/DetailNewsActivity.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/views/Activities/DetailNewsActivity.java
@@ -1,15 +1,15 @@
 package com.example.rishabhkhanna.peopleword.views.Activities;
 
 import android.animation.Animator;
-import android.content.ActivityNotFoundException;
+
 import android.content.Intent;
-import android.content.IntentFilter;
+
 import android.graphics.Bitmap;
-import android.media.Image;
+
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Environment;
+
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
@@ -23,24 +23,20 @@ import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.rishabhkhanna.peopleword.R;
 
 import com.example.rishabhkhanna.peopleword.model.NewsJson;
-import com.example.rishabhkhanna.peopleword.model.PhotoStory;
+
 import com.example.rishabhkhanna.peopleword.utils.Constants;
+
 import com.google.gson.Gson;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
+
 
 public class DetailNewsActivity extends AppCompatActivity {
 
