2ebd92291ef218efe2143a1e68a8c0d9bc007789
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-14 01:40:30 +0530

    API changes in url
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/Network/API.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Network/API.java
@@ -13,7 +13,7 @@ public class API {
 
     private API() {
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.33:9890/")
+                .baseUrl("http://192.168.1.36:9890/")
                 .addConverterFactory(
                         GsonConverterFactory.create()
                 )
--- app/src/main/java/com/example/rishabhkhanna/peopleword/Network/NewsAPI.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Network/NewsAPI.java
@@ -15,7 +15,7 @@ public class NewsAPI {
 
     private NewsAPI() {
         getNews = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.33:9890/")
+                .baseUrl("http://192.168.1.36:9890/")
                 .addConverterFactory(
                         GsonConverterFactory.create()
                 )
