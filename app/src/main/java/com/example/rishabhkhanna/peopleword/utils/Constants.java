454323fd02ca3d03c178088a383ee3e2573156e9
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-14 01:54:08 +0530

    profile constants added
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/utils/Constants.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/utils/Constants.java
@@ -10,22 +10,6 @@ public class Constants {
     public static final String USER_TOPICS_LIST = "user_list";
     public static final String USER_TOPIC_POS = "user_topics";
 
-//    urls
-    public static final String BRIEFS_URL = "http://139.59.35.58:90/test/fetch/briefs/";
-    public static final String TOP_URL = "http://139.59.35.58:90/test/fetch/top_news/";
-    public static final String EVENTS_URL = "http://139.59.35.58:90/test/fetch/events/";
-    public static final String INDIA_URL = "http://139.59.35.58:90/test/fetch/india/";
-    public static final String WORLD_URL = "http://139.59.35.58:90/test/fetch/world/";
-    public static final String SPORTS_URL = "http://139.59.35.58:90/test/fetch/sports/";
-    public static final String CRICKET_URL = "http://139.59.35.58:90/test/fetch/cricket/";
-    public static final String BUSINESS_URL = "http://139.59.35.58:90/test/fetch/business/";
-    public static final String ENTERTAINMENT_URL = "http://139.59.35.58:90/test/fetch/entertainment/";
-    public static final String EDUCATION_URL = "http://139.59.35.58:90/test/fetch/education/";
-    public static final String ENVIRONMENT_URL = "http://139.59.35.58:90/test/fetch/environment/";
-    public static final String TV_URL = "http://139.59.35.58:90/test/fetch/tv/";
-    public static final String AUTOMOTIVE_URL = "http://139.59.35.58:90/test/fetch/automotive/";
-    public static final String LIFESTYLE_URL = "http://139.59.35.58:90/test/fetch/life_style/";
-    public static final String GOODGOV_URL = "http://139.59.35.58:90/test/fetch/good_governance/";
 //    url end
     public static final String FULL_STORY_KEY = "fullstory";
     public static final String SECTION_TITLE = "title";
@@ -39,9 +23,9 @@ public class Constants {
 
     //SharedPrefs DB name
     public static final String AUTH_DETAILS = "newsapp";
+    public static final String AUTH_EMAIL = "emmail";
 
     //psfs News Topic Position Constants
-
     public static final String BRIEF_POS = "brief_pos";
     public static final String TOP_POS = "top_pos";
     public static final String INDIA_POS = "india_pos";
@@ -58,13 +42,11 @@ public class Constants {
     public static final String GOODGOV_POS = "goodgov_pos";
 
     //Postition DB name
-
     public static final String TOPIC_POS_DB = "topic_pos";
-
-    //
 
     public static final String ALL_TOPIC = "all_topic";
 
-
+    //Profile pager link
+    public static final String PROFILE_NEWS = "profile_news";
 
 }
