9411e1369245ff6c1620e33a90afa592d953c68d
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-15 10:01:19 +0530

    removed chat from profile page
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/Adapters/ProfileViewPagerAdapter.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Adapters/ProfileViewPagerAdapter.java
@@ -18,24 +18,22 @@ public class ProfileViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-         ProfileNewsFragment profileNewsFragment = ProfileNewsFragment.getInstance(position);
+        ProfileNewsFragment profileNewsFragment = ProfileNewsFragment.getInstance(position);
         return profileNewsFragment;
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 2;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position){
+        switch (position) {
             case 0:
                 return "Likes";
             case 1:
                 return "Dislikes";
-            case 2:
-                return "Chats";
         }
         return super.getPageTitle(position);
     }
