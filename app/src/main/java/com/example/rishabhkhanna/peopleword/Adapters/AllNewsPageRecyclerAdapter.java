8842193d5fd9bb0a5e4951d914c5d634eb120b94
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-16 09:45:16 +0530

    like dislike from all news page
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/Adapters/AllNewsPageRecyclerAdapter.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Adapters/AllNewsPageRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.BoolRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
@@ -12,21 +13,30 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.rishabhkhanna.peopleword.Network.API;
+import com.example.rishabhkhanna.peopleword.Network.interfaces.rateNews;
 import com.example.rishabhkhanna.peopleword.R;
+import com.example.rishabhkhanna.peopleword.model.AuthResponse;
 import com.example.rishabhkhanna.peopleword.model.NewsJson;
 import com.example.rishabhkhanna.peopleword.model.ToiJson;
 import com.example.rishabhkhanna.peopleword.utils.Constants;
 import com.example.rishabhkhanna.peopleword.views.Activities.DetailNewsActivity;
+import com.facebook.AccessToken;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Random;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by rishabhkhanna on 19/06/17.
@@ -44,6 +54,7 @@ public class AllNewsPageRecyclerAdapter extends RecyclerView.Adapter<AllNewsPage
 
     @Override
     public int getItemViewType(int position) {
+        Log.d(TAG, "getItemViewType: position : " + position);
         if(newsArrayList.size() > 0) {
             if(newsArrayList.get(position).getmUser() != null && !newsArrayList.get(position).getmUser().isEmpty()) {
                 if (newsArrayList.get(position).getmUser().get(0).getmUserTable().getRating() == 1) {
@@ -58,6 +69,7 @@ public class AllNewsPageRecyclerAdapter extends RecyclerView.Adapter<AllNewsPage
 
     @Override
     public AllnewsViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: ViewType: " + viewType);
         LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         int layout = R.layout.news_list_view;
         if(viewType == 1) {
@@ -70,12 +82,46 @@ public class AllNewsPageRecyclerAdapter extends RecyclerView.Adapter<AllNewsPage
     }
 
     @Override
-    public void onBindViewHolder(final AllnewsViewholder holder, int position) {
+    public void onBindViewHolder(final AllnewsViewholder holder, final int position) {
         final NewsJson thisJsonData = newsArrayList.get(position);
         holder.tvNewsHeading.setText(thisJsonData.getHl());
         holder.tvLikes.setText(String.valueOf(thisJsonData.getLikes()));
         holder.tvDislikes.setText(String.valueOf(thisJsonData.getDislikes()));
         holder.tvChats.setText(String.valueOf(thisJsonData.getChats()));
+
+        if(AccessToken.getCurrentAccessToken() != null) {
+            holder.ivLike.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    Log.d(TAG, "onTouch: touch kiya re");
+                    like(thisJsonData,position);
+                    return true;
+                }
+            });
+
+            holder.ivDislike.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    dislike(thisJsonData,position);
+                    return true;
+                }
+            });
+            holder.tvLikes.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    like(thisJsonData,position);
+                    return true;
+                }
+            });
+            holder.tvDislikes.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    dislike(thisJsonData,position);
+                    return true;
+                }
+            });
+        }
+
         Picasso.with(context).load("http://timesofindia.indiatimes.com/thumb.cms?photoid=" +
                 thisJsonData.getImageid() + "&width=600&height=500&resizemode=1")
                 .noFade()
@@ -137,6 +183,65 @@ public class AllNewsPageRecyclerAdapter extends RecyclerView.Adapter<AllNewsPage
             cvNewsList = (CardView) itemView.findViewById(R.id.news_list_cv);
             ivLike = (ImageView) itemView.findViewById(R.id.ivLike);
             ivDislike = (ImageView) itemView.findViewById(R.id.ivDislike);
+
         }
+    }
+
+    public void like(NewsJson thisJsonData, final int position){
+        String token = AccessToken.getCurrentAccessToken().getToken();
+        String userID = AccessToken.getCurrentAccessToken().getUserId();
+        API.getInstance()
+                .retrofit
+                .create(rateNews.class)
+                .likeNews(
+                        token,
+                        userID,
+                        thisJsonData.getMsid(),
+                        String.valueOf(thisJsonData.getId()),
+                        thisJsonData.getmUser().size() >  0 ? thisJsonData.getmUser().get(0).getmUserTable().getRating() : -1
+                ).enqueue(new Callback<NewsJson>() {
+            @Override
+            public void onResponse(Call<NewsJson> call, Response<NewsJson> response) {
+                Log.d(TAG, "onResponse: Cool Your Rating has been recorded");
+                Log.d(TAG, "onResponse: " + call.request());
+                newsArrayList.set(position,response.body());
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<NewsJson> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + call.request());
+            }
+        });
+
+    }
+
+    public void dislike(NewsJson thisJsonData, final int position){
+        String token = AccessToken.getCurrentAccessToken().getToken();
+        String userID = AccessToken.getCurrentAccessToken().getUserId();
+        API.getInstance()
+                .retrofit
+                .create(rateNews.class)
+                .dislikeNews(
+                        token,
+                        userID,
+                        thisJsonData.getMsid(),
+                        String.valueOf(thisJsonData.getId()),
+                        thisJsonData.getmUser().size() > 0 ? thisJsonData.getmUser().get(0).getmUserTable().getRating() : -1
+                ).enqueue(new Callback<NewsJson>() {
+            @Override
+            public void onResponse(Call<NewsJson> call, Response<NewsJson> response) {
+                Log.d(TAG, "onResponse: Cool Your Rating has been recorded");
+                Log.d(TAG, "onResponse: " + call.request());
+                Log.d(TAG, "onResponse: " + response);
+                newsArrayList.set(position,response.body());
+                notifyDataSetChanged();
+
+            }
+            @Override
+            public void onFailure(Call<NewsJson> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + call.request());
+            }
+        });
     }
 }
