22085539d09c3592a0c9ad928a3d9cbbcd2a8d86
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-16 13:07:04 +0530

    expandable textview in photstory adapter
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/Adapters/PhotoStoryAdapter.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Adapters/PhotoStoryAdapter.java
@@ -1,7 +1,9 @@
 package com.example.rishabhkhanna.peopleword.Adapters;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,8 @@ import com.example.rishabhkhanna.peopleword.views.Activities.PhotoStoryActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+
+import at.blogc.android.views.ExpandableTextView;
 
 /**
  * Created by rishabhkhanna on 15/07/17.
@@ -39,16 +43,33 @@ public class PhotoStoryAdapter extends RecyclerView.Adapter<PhotoStoryAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(ViewHolderPhoto holder, int position) {
+    public void onBindViewHolder(final ViewHolderPhoto holder, int position) {
         PhotoStory thisPhotstory = photoStoryArrayList.get(position);
-        holder.tvPhotostory.setText(thisPhotstory.getCap());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            holder.tvPhotostory.setText(Html.fromHtml(thisPhotstory.getCap(),Html.FROM_HTML_MODE_COMPACT));
+        }else{
+            holder.tvPhotostory.setText(Html.fromHtml(thisPhotstory.getCap()));
+        }
         Picasso.with(context).
                 load(UtilMethods.getImageurl(thisPhotstory.getId()))
+                .placeholder(R.drawable.photo_placeholder)
                 .into(holder.ivPhotostory);
         holder.tvPhotostory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+
+        holder.tvPhotostory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.tvPhotostory.isExpanded()){
+                    holder.tvPhotostory.collapse();
+
+                }else{
+                    holder.tvPhotostory.expand();
+                }
             }
         });
 
@@ -61,11 +82,11 @@ public class PhotoStoryAdapter extends RecyclerView.Adapter<PhotoStoryAdapter.Vi
 
     public class ViewHolderPhoto extends RecyclerView.ViewHolder{
         ImageView ivPhotostory;
-        TextView tvPhotostory;
+        ExpandableTextView tvPhotostory;
         public ViewHolderPhoto(View itemView) {
             super(itemView);
             ivPhotostory = (ImageView) itemView.findViewById(R.id.ivPhotostory);
-            tvPhotostory = (TextView) itemView.findViewById(R.id.tvPhotoStory);
+            tvPhotostory = (ExpandableTextView) itemView.findViewById(R.id.tvPhotostory);
         }
     }
 }
