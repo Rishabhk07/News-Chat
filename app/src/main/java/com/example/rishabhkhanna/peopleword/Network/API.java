7ba09c243275b70d1ef4b11e35db4b74be73ee4e
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-20 13:49:38 +0530

    set profile and handle current fragment  when login and logout
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/Network/API.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/Network/API.java
@@ -13,7 +13,7 @@ public class API {
 
     private API() {
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.39:9890/")
+                .baseUrl("http://192.168.0.128:9890/")
                 .addConverterFactory(
                         GsonConverterFactory.create()
                 )
--- app/src/main/java/com/example/rishabhkhanna/peopleword/views/Activities/BaseActivity.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/views/Activities/BaseActivity.java
@@ -4,36 +4,32 @@ package com.example.rishabhkhanna.peopleword.views.Activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+
+
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
-import android.util.DisplayMetrics;
+
 import android.util.Log;
-import android.view.Display;
+
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.android.volley.VolleyError;
-import com.daprlabs.cardstack.SwipeDeck;
-import com.example.rishabhkhanna.peopleword.Adapters.RateNewsAdapter;
-import com.example.rishabhkhanna.peopleword.Interfaces.onJsonRecieved;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
 import com.example.rishabhkhanna.peopleword.R;
-import com.example.rishabhkhanna.peopleword.model.ToiJson;
+
 import com.example.rishabhkhanna.peopleword.utils.Constants;
-import com.example.rishabhkhanna.peopleword.utils.FetchNews;
+
 import com.example.rishabhkhanna.peopleword.views.Fragments.AllNewsFragment;
 import com.example.rishabhkhanna.peopleword.views.Fragments.LoginFragment;
 import com.example.rishabhkhanna.peopleword.views.Fragments.NewsTopic;
@@ -41,15 +37,11 @@ import com.example.rishabhkhanna.peopleword.views.Fragments.ProfileFragment;
 import com.example.rishabhkhanna.peopleword.views.Fragments.RateNewFragment;
 import com.example.rishabhkhanna.peopleword.views.Fragments.YourNewsFragment;
 import com.facebook.AccessToken;
-import com.facebook.FacebookSdk;
+
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.squareup.picasso.Picasso;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
-import java.util.ArrayList;
 
 import io.realm.Realm;
 
@@ -61,8 +53,137 @@ public class BaseActivity extends AppCompatActivity
     String thisTab = null;
     Boolean ontop = false;
     ImageView headerImageView;
+    TextView tvHeaderName;
+    TextView tvHeaderMail;
     NavigationView navigationView;
     int loginRecreate = 0;
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        thisTab = intent.getStringExtra("notification_key");
+        loginRecreate = intent.getIntExtra(Constants.loginFragmentIntent, 0);
+
+//
+//        Log.d(TAG, "onNewIntent: " + thisTab);
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        Log.d(TAG, "onCreate: " + thisTab);
+//        if (thisTab != null) {
+//            fragment = AllNewsFragment.getInstance(thisTab);
+//        } else {
+//            fragment = new AllNewsFragment();
+//        }
+//        fragmentTransaction.replace(R.id.flActivity_main, fragment).commitAllowingStateLoss();
+
+        Log.d(TAG, "onNewIntent: Recreate" + loginRecreate);
+        setFragment(thisTab);
+        setProfilePicture();
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ontop = true;
+        setContentView(R.layout.activity_nav_drawer);
+        Log.d(TAG, "onCreate: ");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        thisTab = getIntent().getStringExtra("notification_key");
+//        loginRecreate = getIntent().getIntExtra(Constants.loginFragmentIntent,0);
+        Log.d(TAG, "onCreate: loginrEcreate: " + loginRecreate);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        Realm.init(this);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        Log.d(TAG, "onCreate: " + thisTab);
+        setFragment(thisTab);
+        setProfilePicture();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.AUTH_DETAILS, Context.MODE_PRIVATE);
+        String token = sharedPreferences.getString(Constants.LOGIN_TOKEN, "null");
+        int loginPage = 0;
+        if (id == R.id.nav_rate_news) {
+            if (token.equals("null") || AccessToken.getCurrentAccessToken() == null) {
+                loginPage = 1;
+                getLoginPage(loginPage);
+                setTitle("Login");
+            } else {
+                fragment = new RateNewFragment();
+                setTitle("Rate News");
+            }
+
+        } else if (id == R.id.allNews) {
+            fragment = new AllNewsFragment();
+            setTitle("All News");
+        } else if (id == R.id.nav_Topic) {
+            if (token.equals("null") || AccessToken.getCurrentAccessToken() == null) {
+                loginPage = 3;
+                getLoginPage(loginPage);
+                setTitle("Login");
+            } else {
+                fragment = new NewsTopic();
+                setTitle("Topics");
+            }
+        } else if (id == R.id.nav_your_news) {
+            if (token.equals("null") || AccessToken.getCurrentAccessToken() == null) {
+                loginPage = 2;
+                getLoginPage(loginPage);
+                setTitle("Login");
+            } else {
+                fragment = new YourNewsFragment();
+                setTitle("Your News");
+            }
+        } else if (id == R.id.nav_signout) {
+            LoginManager.getInstance().logOut();
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(Constants.LOGIN_TOKEN, "null");
+            editor.putString(Constants.LOGIN_USER_ID, "null");
+            editor.apply();
+            loginRecreate = 0;
+            setFragment(null);
+            setProfilePicture();
+        } else if (id == R.id.nav_profile) {
+            Log.d(TAG, "onNavigationItemSelected: " + id);
+            navigationView.getMenu().findItem(id).setChecked(true);
+            if (token.equals("null") || AccessToken.getCurrentAccessToken() == null) {
+                loginPage = 4;
+                getLoginPage(loginPage);
+                setTitle("Login");
+            } else {
+                fragment = new ProfileFragment();
+                setTitle("Profile");
+            }
+        }
+        fragmentTransaction.replace(R.id.flActivity_main, fragment).commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     @Override
     protected void onStop() {
         Log.d(TAG, "onStop: ");
@@ -84,143 +205,20 @@ public class BaseActivity extends AppCompatActivity
         super.onStart();
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        thisTab = intent.getStringExtra("notification_key");
-        loginRecreate = intent.getIntExtra(Constants.loginFragmentIntent,0);
-
-//
-//        Log.d(TAG, "onNewIntent: " + thisTab);
-//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        Log.d(TAG, "onCreate: " + thisTab);
-//        if (thisTab != null) {
-//            fragment = AllNewsFragment.getInstance(thisTab);
-//        } else {
-//            fragment = new AllNewsFragment();
-//        }
-//        fragmentTransaction.replace(R.id.flActivity_main, fragment).commitAllowingStateLoss();
-
-        Log.d(TAG, "onNewIntent: Recreate" + loginRecreate);
-
-            setFragment(thisTab);
-
-
-        super.onNewIntent(intent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ontop = true;
-        Log.d(TAG, "onCreate: ");
-        setContentView(R.layout.activity_nav_drawer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        thisTab = getIntent().getStringExtra("notification_key");
-//        loginRecreate = getIntent().getIntExtra(Constants.loginFragmentIntent,0);
-        Log.d(TAG, "onCreate: loginrEcreate: " + loginRecreate );
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        Realm.init(this);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        Log.d(TAG, "onCreate: " + thisTab);
-        setFragment(null);
-        if (AccessToken.getCurrentAccessToken() != null) {
-            navigationView = (NavigationView) findViewById(R.id.nav_view);
-            View headerView = navigationView.getHeaderView(0);
-            headerImageView = (ImageView) headerView.findViewById(R.id.headerImageView);
-            Picasso.with(this).load(Profile.getCurrentProfile().getProfilePictureUri(100, 100)).into(headerImageView);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        SharedPreferences sharedPreferences = getSharedPreferences(Constants.AUTH_DETAILS, Context.MODE_PRIVATE);
-        String token = sharedPreferences.getString(Constants.LOGIN_TOKEN,"null");
-        int loginPage = 0;
-        if (id == R.id.nav_rate_news) {
-            if(token.equals("null") || AccessToken.getCurrentAccessToken() == null){
-                loginPage = 1;
-                getLoginPage(loginPage);
-            }else{
-                fragment = new RateNewFragment();
-                setTitle("Rate News");
-            }
-
-        } else if (id == R.id.allNews) {
-                fragment = new AllNewsFragment();
-                setTitle("All News");
-        } else if (id == R.id.nav_Topic) {
-            if(token.equals("null") || AccessToken.getCurrentAccessToken() == null){
-                loginPage = 3;
-                getLoginPage(loginPage);
-            }else {
-                fragment = new NewsTopic();
-                setTitle("Topics");
-            }
-        } else if (id == R.id.nav_your_news) {
-            if(token.equals("null") || AccessToken.getCurrentAccessToken() == null){
-                loginPage = 2;
-                getLoginPage(loginPage);
-            }else {
-                fragment = new YourNewsFragment();
-                setTitle("Your News");
-            }
-        } else if (id == R.id.nav_signout) {
-            LoginManager.getInstance().logOut();
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString(Constants.LOGIN_TOKEN, "null");
-            editor.putString(Constants.LOGIN_USER_ID, "null");
-            editor.apply();
-            loginRecreate = 0;
-            setFragment(null);
-        } else if (id == R.id.nav_edit) {
-            if(token.equals("null") || AccessToken.getCurrentAccessToken() == null){
-                loginPage = 4;
-                getLoginPage(loginPage);
-            }else {
-                fragment = new ProfileFragment();
-                setTitle("Profile");
-            }
-        }
-        fragmentTransaction.replace(R.id.flActivity_main, fragment).commit();
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
     private void getLoginPage(int page) {
-        fragment = LoginFragment.newInstance("Login Here",page);
+        fragment = LoginFragment.newInstance("Login Here", page);
     }
 
-    private void setFragment(String thisTab){
+    private void setFragment(String thisTab) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        switch(loginRecreate){
+        switch (loginRecreate) {
             case 0:
                 if (thisTab != null) {
                     fragment = AllNewsFragment.getInstance(thisTab);
                 } else {
                     fragment = new AllNewsFragment();
                 }
+                navigationView.getMenu().getItem(0).setChecked(true);
                 setTitle("All News");
                 break;
             case 1:
@@ -240,8 +238,29 @@ public class BaseActivity extends AppCompatActivity
                 setTitle("Profile");
                 break;
         }
-
         fragmentTransaction.replace(R.id.flActivity_main, fragment).commitAllowingStateLoss();
+
     }
 
+    public void setProfilePicture() {
+        View headerView = navigationView.getHeaderView(0);
+        headerImageView = (ImageView) headerView.findViewById(R.id.headerImageView);
+        tvHeaderName = (TextView) headerView.findViewById(R.id.tvHeaderName);
+        tvHeaderMail = (TextView) headerView.findViewById(R.id.tvHeaderMail);
+        if (AccessToken.getCurrentAccessToken() != null) {
+            Picasso.with(this).load(Profile.getCurrentProfile().getProfilePictureUri(100, 100)).into(headerImageView);
+            tvHeaderName.setText(Profile.getCurrentProfile().getName());
+            SharedPreferences sharedPreferences = getSharedPreferences(Constants.AUTH_DETAILS, MODE_PRIVATE);
+            String email = sharedPreferences.getString(Constants.AUTH_EMAIL, "");
+            tvHeaderMail.setVisibility(View.VISIBLE);
+            tvHeaderMail.setText(email);
+
+        } else {
+            Picasso.with(this).load(R.drawable.person_placeholder).into(headerImageView);
+            tvHeaderName.setText("Guest");
+            tvHeaderMail.setVisibility(View.GONE);
+
+        }
+
+    }
 }
