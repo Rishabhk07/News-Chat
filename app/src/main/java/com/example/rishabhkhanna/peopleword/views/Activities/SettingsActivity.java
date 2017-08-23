a2d6f74e48626b77abcb454248b9e42103ff23f5
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-23 18:07:11 +0530

    settings Activity
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/example/rishabhkhanna/peopleword/views/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/rishabhkhanna/peopleword/views/Activities/SettingsActivity.java
@@ -1,15 +1,86 @@
 package com.example.rishabhkhanna.peopleword.views.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.rishabhkhanna.peopleword.R;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.io.ObjectStreamException;
 
+public class SettingsActivity extends PreferenceActivity{
+    public static final String TAG = "Setting Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.prefrences);
+        setTitle("Settings");
+        final CheckBoxPreference notification = (CheckBoxPreference) findPreference("notification");
+        final CheckBoxPreference topicNotification = (CheckBoxPreference) findPreference("topic_notification");
+//        notification.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+//            @Override
+//            public boolean onPreferenceChange(Preference preference, Object newValue) {
+//                Log.d(TAG, "onPreferenceChange: " + newValue);
+//                if((Boolean)newValue) {
+//                    topicNotification.setChecked(false);
+//                }else {
+//                    Log.d(TAG, "onPreferenceChange: TOPIC IN NOTI" + topicNotification.isChecked());
+//                    if(!topicNotification.isChecked()){
+//                        notification.setChecked(true);
+//                        newValue = true;
+//                    }
+//                }
+//                return true;
+//            }
+//        });
+
+        notification.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if(notification.isChecked()){
+                    topicNotification.setChecked(false);
+                }else {
+                    if(!topicNotification.isChecked()){
+                        notification.setChecked(true);
+                    }
+                }
+                return false;
+            }
+        });
+
+        topicNotification.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if(topicNotification.isChecked()){
+                    notification.setChecked(false);
+                }else{
+                    if(!notification.isChecked()){
+                        topicNotification.setChecked(true);
+                    }
+                }
+                return false;
+            }
+        });
+//        topicNotification.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+//            @Override
+//            public boolean onPreferenceChange(Preference preference, Object newValue) {
+//                Log.d(TAG, "onPreferenceChange: TOPIC" + newValue);
+//                if((Boolean)newValue){
+//                    notification.setChecked(false);
+//                }else {
+//                    if(!notification.isChecked()){
+//                        topicNotification.setChecked(true);
+//                        newValue = true;
+//                    }
+//                }
+//                return true;
+//            }
+//        });
+
     }
+
+
 }
