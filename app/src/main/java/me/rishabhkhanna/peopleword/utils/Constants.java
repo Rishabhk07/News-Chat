2efb38f67f8af31a38696878703e2e54c04fdc27
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-09-06 22:54:07 +0530

    first time user key and db
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/peopleword/utils/Constants.java
+++ app/src/main/java/me/rishabhkhanna/peopleword/utils/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String USER_TOPICS_LIST = "user_list";
     public static final String USER_TOPIC_POS = "user_topics";
 
-//    url end
+    //    url end
     public static final String FULL_STORY_KEY = "fullstory";
     public static final String SECTION_TITLE = "title";
     public static final String LOGIN_TOKEN = "login_token";
@@ -25,21 +25,6 @@ public class Constants {
     public static final String AUTH_DETAILS = "newsapp";
     public static final String AUTH_EMAIL = "emmail";
 
-    //psfs News Topic Position Constants
-    public static final String BRIEF_POS = "brief_pos";
-    public static final String TOP_POS = "top_pos";
-    public static final String INDIA_POS = "india_pos";
-    public static final String WORLD_POS = "world_pos";
-    public static final String SPORTS_POS = "sports_pos";
-    public static final String CRICKET_POS = "cricket_pos";
-    public static final String BUSINESS_POS = "business_pos";
-    public static final String ENTERTAIN_POS = "entertainment_pos";
-    public static final String EDUCATION_POS = "education_pos";
-    public static final String ENVIRONMENT_POS = "environ_pos";
-    public static final String TV_POS = "tv_pos";
-    public static final String AUTOMOTIVE_POS = "automotive_pos";
-    public static final String LIFESTYLE_POS = "lifestyle_pos";
-    public static final String GOODGOV_POS = "goodgov_pos";
 
     //Postition DB name
     public static final String TOPIC_POS_DB = "topic_pos";
@@ -53,5 +38,9 @@ public class Constants {
     public static final String LOGIN_STRING = "login_string";
     public static final String loginFragmentIntent = "login_fragment";
     public static final String loginPage = "login_page";
+
+    // First time user key
+    public static final String firstUserKey = "user_key";
+    public static final String first_user_DB = "general_db";
 
 }
