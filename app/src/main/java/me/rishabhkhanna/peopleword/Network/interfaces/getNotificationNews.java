53bec100a200860daa130822e14b7db762539946
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-09-13 00:30:53 +0530

    request particular news from id
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/peopleword/Network/interfaces/getNotificationNews.java
+++ app/src/main/java/me/rishabhkhanna/peopleword/Network/interfaces/getNotificationNews.java
@@ -1,8 +1,23 @@
 package me.rishabhkhanna.peopleword.Network.interfaces;
 
+import me.rishabhkhanna.peopleword.model.NewsJson;
+import me.rishabhkhanna.peopleword.utils.UrlConstants;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 /**
  * Created by rishabhkhanna on 12/09/17.
  */
 
-public class getNotificationNews {
+public interface getNotificationNews {
+
+    @GET(UrlConstants.NOTIFY + "/{msid}/{news_id}")
+    Call<NewsJson> getThisNews(
+            @Path("msid") String table_key,
+            @Path("news_id") String news_id
+    );
 }
