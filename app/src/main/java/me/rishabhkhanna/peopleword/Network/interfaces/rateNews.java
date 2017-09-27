81edbfc1ac19818d268355bc424deccac695287d
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-09-28 00:03:22 +0530

    offset query on rate news call
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/peopleword/Network/interfaces/rateNews.java
+++ app/src/main/java/me/rishabhkhanna/peopleword/Network/interfaces/rateNews.java
@@ -53,7 +53,8 @@ public interface rateNews {
     @FormUrlEncoded
     @POST("rate/getNews")
     Call<ArrayList<ArrayList<NewsJson>>> getNews(
-            @Field("user_id") String userId
+            @Field("user_id") String userId,
+            @Query("offset") int offset
     );
 
 }
